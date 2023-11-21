56c667f8630241048d5b82cf460683550d4a7f40
Michael Jones <michaeljones@Michaels-MacBook-Pro.local>
2023-11-21 00:20:34 -0500

    update

--- lab4/password_generator.py
+++ lab4/password_generator.py
@@ -1,3 +1,17 @@
+# =================================================================
+#
+# Authors: Michael Jones <mjones467@student.umgc.edu>
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# =================================================================
 """
 This module provides functionality to generate a password, optionally salt it,
 hash it using a selected algorithm, and write the password details to a CSV file.
@@ -66,10 +80,6 @@ def hash_password(password, algorithm):
         return hash_function(password)
     except (TypeError, ValueError) as e:
         raise ValueError(f"Hashing error due to input: {str(e)}")
-    except (bcrypt.exceptions.BcryptError, 
-            scrypt.error, 
-            argon2.exceptions.Argon2Error) as e:
-        raise Exception(f"Error in hashing library: {str(e)}")
 
 
 def main():
